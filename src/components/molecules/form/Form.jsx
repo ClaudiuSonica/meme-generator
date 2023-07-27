7831778e181aef68d8cb4638fc077bb065094dc6
Claudiu Sonica <csonica95@gmail.com>
2023-07-27 17:13:32 +0200

    button fixed

--- src/components/molecules/form/Form.jsx
+++ src/components/molecules/form/Form.jsx
@@ -1,15 +1,29 @@
-import './Form.scss';
+import "./Form.scss";
 
 const Form = () => {
+
+  const preventDefault = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <form className='form'>
-      <div className="form--input__box">
-        <input className='form--input' type="text" />
-        <input className='form--input' type="text" />
-      </div>
-      <button className='form--btn' >Get a new meme image 🖼</button>
-    </form>
-  )
-}
+    <main>
+      <form className="form" onSubmit={preventDefault}>
+        <div className="form--input__box">
+          <input
+            className="form--input"
+            type="text"
+            placeholder="Top text" />
+          <input
+            className="form--input"
+            type="text"
+            placeholder="Bottom text"
+          />
+        </div>
+        <button className="form--btn">Get a new meme image 🖼</button>
+      </form>
+    </main>
+  );
+};
 
 export default Form;
